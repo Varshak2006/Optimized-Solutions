338a596952a561d3e17443d1dd5960e4eb60af94
Varsha-kanakdande <kanakdandevarsha@gmail.com>
2026-03-16 22:46:31 +0530

    Add insertion sort for singly-linked list
    Implement insertion sort for a singly-linked list using a temporary vector to store values.
    Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
    
    The steps of the insertion sort algorithm:
    
    Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
    At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
    It repeats until no input elements remain.

---
+++ sorting/Insertion_sort_list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+    ListNode* temp=head;
+    vector<int>v;
+    v.push_back(temp->val);
+    while(temp->next!=nullptr){
+       
+        temp=temp->next;
+         v.push_back(temp->val);
+    }
+  sort(v.begin(),v.end());
+    if (v.empty()) return NULL;
+
+    ListNode* header = new ListNode(v[0]);
+     ListNode* tail = header;
+
+    for (int i = 1; i < v.size(); i++) {
+        tail->next = new ListNode(v[i]);
+        tail = tail->next;
+    }
+
+    return header;
+}
+
+  
+
+};
